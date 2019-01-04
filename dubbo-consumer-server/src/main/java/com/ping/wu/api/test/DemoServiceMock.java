0d4897b510d0b7c8d33b8481e4b449991e1f15b1
wp <403704156@qq.com>
2019-01-05 07:18:22 +0800

    initial

---
+++ dubbo-consumer-server/src/main/java/com/ping/wu/api/test/DemoServiceMock.java
@@ -0,0 +1,24 @@
+package com.ping.wu.api.test;
+
+/**
+ * @author wuping
+ * @date 2019/1/5
+ */
+
+public class DemoServiceMock implements DemoService {
+
+    @Override
+    public void saySomething() {
+
+    }
+
+    @Override
+    public void reply(String s) {
+
+    }
+
+    @Override
+    public String testMock() {
+        return "触发熔断啦!";
+    }
+}
